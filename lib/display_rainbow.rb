02159012041ec3d2e5216cd340836031feef8b32
Amari Ellington <amari.l.ellington@gmail.com>
2019-02-12 19:08:49 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,6 +3,6 @@ def display_rainbow(colors)
 
   colors=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-  print "R: (display_rainbow[0]), O: (display_rainbow[1], Y: (display_rainbow[2]), G: (display_rainbow[3], B: (display_rainbow[4], I: (display_rainbow[5], V: (display_rainbow[6] "
-
 end
+
+print "R: (display_rainbow[0]), O: (display_rainbow[1], Y: (display_rainbow[2]), G: (display_rainbow[3], B: (display_rainbow[4], I: (display_rainbow[5], V: (display_rainbow[6]""
